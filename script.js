ca156b659ecd6f936b47acbe00d2fe766774643f
MayurSeth <78924577+MayurSeth@users.noreply.github.com>
2021-02-11 22:44:16 +0530

    myWeather

---
+++ script.js
@@ -0,0 +1,23 @@
+$(() => {
+
+
+
+    $('#btn').click(() => {
+        let city = $('#inp').val()
+        let temperature;
+        const unit="metric";
+        console.log(city)
+         let res = $.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid={Your Api Id}&units=${unit}`, (data) => {
+            temperature = data.main.temp
+            console.log(temperature)
+            $('.box_1').append(
+                $(`<div>`).text(temperature)
+
+            )
+
+
+        })
+
+
+    })
+})
